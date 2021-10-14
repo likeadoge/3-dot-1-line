520d2291cf1d953359594bf725d805a76623d360
undefined <undefined>
2021-10-14 18:15:27 +0800

    init

--- src/game/Display.ts
+++ src/game/Display.ts
@@ -1,5 +1,3 @@
-import { Point } from '@/base/Point'
-import { ResImg } from '@/res/ResImg'
 import { GameOpiton } from './Game'
 export class Display {
     height: number
@@ -16,8 +14,6 @@ export class Display {
         this.canvas.height = this.height
         this.canvas.width = this.width
 
-        console.log(this.cntr)
-
         this.cntr.appendChild(this.canvas)
 
         const ctx = this.canvas.getContext('2d')
@@ -28,35 +24,100 @@ export class Display {
             this.ctx = ctx
 
     }
-
-
-    dot(point: Point, color: [number, number, number, number] = [255, 255, 255, 0.3], size: number = 3) {
-        const ctx = this.ctx
-
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, size, 0, Math.PI * 2, true);
-        ctx.fillStyle = `rgba(${color.join(',')})`
-        ctx.fill();
-    }
-
-    img(img: ResImg, p: Point, s: Point = new Point(1)) {
-        const ctx = this.ctx
-
-        ctx.drawImage(
-            img.img.get(),
-            p.x - img.width / 2 * s.x,
-            p.y - img.height / 2 * s.y,
-            s.x * img.width,
-            s.y * img.height
-        )
-    }
-
     clear() {
-        // this.ctx.fillStyle = 'rgba(255,255,255,0.3)';
-        // this.ctx.fillRect(0, 0, this.width, this.height);
         this.ctx.clearRect(0, 0, this.width, this.height)
     }
+}
 
+
+export class GlDisplay extends Display {
+    gl: WebGLRenderingContext
+    constructor(option: GameOpiton) {
+        super(option)
+        const ctx = this.canvas.getContext('webgl')
+
+        if (!ctx)
+            throw new Error('ctx is null !!!')
+        else
+            this.gl = ctx
+    }
+
+    private vsSource = `
+        attribute vec4 aVertexPosition;
+
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+
+        void main() {
+            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        }
+    `
+
+    private fsSource = `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    `
+
+    clear(){
+        const {gl} = this
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+
+
+    draw(){
+
+    }
+
+
+    initShaderProgram() {
+
+        function loadShader(gl:any, type:any, source:any) {
+            const shader = gl.createShader(type);
+          
+            // Send the source to the shader object
+          
+            gl.shaderSource(shader, source);
+          
+            // Compile the shader program
+          
+            gl.compileShader(shader);
+          
+            // See if it compiled successfully
+          
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+              alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+              gl.deleteShader(shader);
+              return null;
+            }
+          
+            return shader;
+          }
+
+        const {gl, vsSource, fsSource} = this
+
+        const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+        const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+      
+        // 创建着色器程序
+      
+        const shaderProgram = gl.createProgram();
+
+        if(!shaderProgram) throw new Error('')
+
+        gl.attachShader(shaderProgram, vertexShader);
+        gl.attachShader(shaderProgram, fragmentShader);
+        gl.linkProgram(shaderProgram);
+      
+        // 创建失败， alert
+        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+          alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+          return null;
+        }
+      
+        return shaderProgram;
+      }
 }
 
 
--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,11 +1,5 @@
-import { RandomPathState } from "@/state/RandomPathState"
-import { Display } from "./Display"
-import { images } from '@/res/ResImg'
-import { Point } from "@/base/Point"
-import { TouchMoveState } from "@/state/TouchMoveState"
-import { TouchTimeState } from "@/state/TouchTimeState"
-import { StatusBarView } from "@/view/StatusBarView"
-import { BackSightView, FrontSightView } from "@/view/SightView"
+
+import { Display, GlDisplay } from "./Display"
 
 export type GameOpiton = { height: number, width: number, cntr: Element }
 
@@ -14,47 +8,21 @@ export class Game {
 
     private option: GameOpiton
 
-    private screen: Display = (null as any)
+    private screen: GlDisplay = (null as any)
 
-    // 目标靶位置
-    private targetPosition = new RandomPathState()
-
-    // 照门相对于目标位置
-    private sightPosition = new RandomPathState()
-
-    // 准星相对于照门位置
-    private frontPosition = new RandomPathState()
-
-    // 视角位置
-    private cameraPosition = new TouchMoveState()
-
-    // 点击状态
-    private touchTime = new TouchTimeState()
-
+   
     // 运行状态
     private isRun = false
-
-    // 姿势状态参数
-    private statusBar: StatusBarView = null as any
-
-    private backSightView: BackSightView = null as any
-
-    private frontSightView: FrontSightView = null as any
 
     constructor(option: GameOpiton) {
         this.option = option
         this.initScreen()
-
-        this.statusBar = new StatusBarView(this.screen)
-        this.backSightView = new BackSightView(this.screen)
-        this.frontSightView = new FrontSightView(this.screen)
-
         this.start()
     }
 
     // 初始化视图
     private initScreen() {
-        this.screen = new Display(this.option)
+        this.screen = new GlDisplay(this.option)
     }
 
     // 绘制图像
@@ -62,50 +30,12 @@ export class Game {
         if (!this.isRun) return
 
         this.screen.clear()
+        this.screen.initShaderProgram()
 
-        const target = this.targetPosition.next().trans(
-            x => x * this.option.width / 2 / 10 + this.option.width / 2,
-            y => y * this.option.height / 2 / 10 + this.option.height / 2,
-        )
-
-
-        const touch = this.cameraPosition.getPoint()
-
-        this.screen.img(
-            images.target,
-            target.add(touch),
-            new Point(0.5)
-        )
-
-        const back = this.sightPosition.next().trans(
-            x => x * this.option.width / 2 / 100 * 4 + this.option.width / 2,
-            x => x * this.option.height / 2 / 100 * 4 + this.option.height / 2
-        )
-        const front = back.add(
-            this.frontPosition.next().trans(
-                x => x * this.option.width / 2 * 2 / 100,
-                y => y * this.option.height / 2 * 2 / 100
-            )
-        )
-        const { focus,trigger } = this.touchTime.num()
-
-        this.screen.dot(
-            front,
-            [0, 255, 0, 1],
-            4
-        )
-        this.frontSightView.draw(front, focus)
-        this.backSightView.draw(back, focus)
-        this.statusBar.draw({focus,trigger})
-
-        requestAnimationFrame(() => this.draw())
+        // requestAnimationFrame(() => this.draw())
     }
 
     async start() {
-        await Promise.all(
-            Array.from(Object.values(images))
-                .map((v: any) => v.onload)
-        )
         this.isRun = true
         this.draw()
     }
